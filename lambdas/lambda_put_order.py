a471e5ae390c7be6ee0905f8024c166bff0a8788
Sonja <sonja.oehlandt@brightstraining.com>
2022-12-15 11:40:07 +0200

    added json.dumps to lambda

--- lambdas/lambda_put_order.py
+++ lambdas/lambda_put_order.py
@@ -4,7 +4,7 @@ import os
 
 #lambda for putting order to dynamoDB
 def lambda_handler(event, context):
-    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=event)
+    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=json.dumps(event))
     response = {
         'statusCode': 200,
         'body': {'lol': 'yes'},
