73ccb2f6494b9f610b7c7df274ee95164788ded3
Sonja <sonja.oehlandt@brightstraining.com>
2022-12-15 16:26:12 +0200

    added a loopty loop

--- lambdas/app.py
+++ lambdas/app.py
@@ -3,11 +3,17 @@ import boto3
 import os
 
 def lambda_handler(event, context):
-  data = boto3.client('dynamodb').scan(TableName=os.environ['TableName'], ExpressionAttributeValues={':a': {'S': 'taikamatto',},}, FilterExpression='type = :a',)
+  data = boto3.client('dynamodb').scan(TableName=os.environ['TableName'])
+
+  Items = []
+
+  for item in data['Items']:
+    if item['type'] == 'taikamatto':
+      Items.append(item)
 
   response = {
       'statusCode': 200,
-      'body': json.dumps(data['Items']),
+      'body': json.dumps(Items),
       'headers': {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
--- lambdas/lambda_put_order.py
+++ lambdas/lambda_put_order.py
@@ -5,6 +5,9 @@ import os
 #lambda for putting order to dynamoDB
 def lambda_handler(event, context):
     data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=json.loads(event['body']))
+    message_to_send = json.dumps(event['body'])
+    client = boto3.client('sns')
+    response = client.publish(TopicArn = os.environ['TopicArn'], Message = message_to_send, Subject = 'Your magic carpet order')
     response = {
         'statusCode': 200,
         'body': json.dumps(event['body']),
