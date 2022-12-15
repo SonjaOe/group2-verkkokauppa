4a43cf47bd9cdc43078885089ac2bf9d46313d28
Sonja <sonja.oehlandt@brightstraining.com>
2022-12-15 10:03:24 +0200

    added put order lambda

---
+++ lambdas/lambda_put_order.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+import os
+
+#lambda for putting order to dynamoDB
+def lambda_handler(event, context):
+    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=event['body'])
+    response = {
+        'statusCode': 200,
+        'body': event['body'],
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
+    return response
