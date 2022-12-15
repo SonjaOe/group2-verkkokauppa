42d7c19b1eeb61d218e9555268caac1930fc2dad
Sonja <sonja.oehlandt@brightstraining.com>
2022-12-15 13:33:32 +0200

    testing lambda handler without dynamodb

--- lambdas/lambda_put_order.py
+++ lambdas/lambda_put_order.py
@@ -4,10 +4,10 @@ import os
 
 #lambda for putting order to dynamoDB
 def lambda_handler(event, context):
-    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=json.dumps(event))
+    #data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=json.dumps(event))
     response = {
         'statusCode': 200,
-        'body': {'lol': 'yes'},
+        'body': json.dumps(event),
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
