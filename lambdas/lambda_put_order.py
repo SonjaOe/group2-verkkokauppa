4c284db18f07f5fc01408b4d08f267a3b4edb980
Sonja <sonja.oehlandt@brightstraining.com>
2022-12-15 10:32:50 +0200

    lambda put order changes for review

--- lambdas/lambda_put_order.py
+++ lambdas/lambda_put_order.py
@@ -4,10 +4,10 @@ import os
 
 #lambda for putting order to dynamoDB
 def lambda_handler(event, context):
-    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=event['body'])
+    # data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=event['body'])
     response = {
         'statusCode': 200,
-        'body': event['body'],
+        'body': event,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
